f919f4bf9311b600bc0502f92a12eb77097c9e45
Developer Nahida <ummenahida20@gmail.com>
2025-08-13 00:42:52 +0600

    added logo in navbar

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/assets/icons/logo"
+import Logo from "@/assets/icons/Logo"
 import { Button } from "@/components/ui/button"
 import {
   NavigationMenu,
@@ -24,7 +24,7 @@ const navigationLinks = [
 export default function Component() {
   return (
     <header className="border-b px-4 md:px-6">
-      <div className="flex h-16 items-center justify-between gap-4">
+      <div className="container flex h-16 items-center justify-between gap-4">
         {/* Left side */}
         <div className="flex items-center gap-2">
           {/* Mobile menu trigger */}
